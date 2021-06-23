a526f3fce32740cea9c787a05a93cc1628c60b24
AoEiuV020 <aoeiuv020@gmail.com>
2021-06-23 22:23:59 +0800

    分离出gn gen脚本

--- script/download_src.sh
+++ script/download_src.sh
@@ -10,7 +10,3 @@ build/linux/sysroot_scripts/install-sysroot.py --arch=i386
 build/linux/sysroot_scripts/install-sysroot.py --arch=amd64
 keytool -genkey -v -keystore keystore.jks -alias dev -keyalg RSA -keysize 2048 -validity 10000 -storepass public_password -keypass public_password -dname "cn=Kiwi Browser (unverified), ou=Actions, o=Kiwi Browser, c=GitHub"
 gclient runhooks
-mkdir -p out/arm64/
-cat .build/android_arm/args.gn | sed "s#target_cpu = \"arm\"#target_cpu = \"arm64\"#" | sed "s#android_default_version_name = \"Git\"#android_default_version_name = \"Git$(date '+%y%m%d')Gen${BUILD_VERSION}\"#" > out/arm64/args.gn
-sed -i "s#android_default_version_code = \"1\"#android_default_version_code = \"$(date '+%y%m%d')2\"#" out/arm64/args.gn
-gn gen out/arm64/
---
+++ script/gen.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+mkdir -p out/arm64/
+cat .build/android_arm/args.gn | sed "s#target_cpu = \"arm\"#target_cpu = \"arm64\"#" | sed "s#android_default_version_name = \"Git\"#android_default_version_name = \"Git$(date '+%y%m%d')Gen${BUILD_VERSION}\"#" > out/arm64/args.gn
+sed -i "s#android_default_version_code = \"1\"#android_default_version_code = \"$(date '+%y%m%d')2\"#" out/arm64/args.gn
+gn gen out/arm64/
