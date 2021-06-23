c10b5d6420212ded4dccffc67b297d70fc4985d4
AoEiuV020 <aoeiuv020@gmail.com>
2021-06-23 21:49:22 +0800

    debugger中创建准备脚本

--- script/download_depot.sh
+++ script/download_depot.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 git clone --depth=1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export DEPOT_TOOLS_DIR=$PWD/depot_tools
+export PATH=$DEPOT_TOOLS_DIR:$DEPOT_TOOLS_DIR/python2_bin:$DEPOT_TOOLS_DIR/python_bin:$PATH
 echo "$DEPOT_TOOLS_DIR" >> $GITHUB_PATH
 echo "$DEPOT_TOOLS_DIR/python2_bin" >> $GITHUB_PATH
 echo "$DEPOT_TOOLS_DIR/python_bin" >> $GITHUB_PATH
